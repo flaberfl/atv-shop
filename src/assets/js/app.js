a319810d3953278df1c6aa487e096e5806350514
Flaber <flaber@Flabers-Mac-Pro.local>
2021-02-09 00:15:05 +0300

    немного поправил js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,40 +1,37 @@
 // Табы для сайдбара
 
 let tabsNav = document.querySelector('.nav') // Родитель линков
-let tabsContent = document.querySelector('.category') // Родитель таб-контента
-let navLink = tabsNav.querySelectorAll('.nav__item')   // Линки
-let items = tabsContent.querySelectorAll('.category__items') // Таб-контент
+let tabsContentNav = document.querySelector('.category') // Родитель таб-контента
+let navLinkNav = tabsNav.querySelectorAll('.nav__item') // Линки
+let itemsTabs = tabsContentNav.querySelectorAll('.category__items') // Таб-контент
 
-function change(arr, i) {
-	arr.forEach( item => {
-		item.forEach( i => {i.classList.remove('is-active')})
-		item[i].classList.add('is-active')
-	})
+// Добавил тег is-active
+function isActive(arr, i) {
+  arr.forEach(item => {
+    item.forEach(i => {
+      i.classList.remove('is-active')
+    })
+    item[i].classList.add('is-active')
+  })
 }
 
-for(let i = 0; i < navLink.length; i++) {
-	navLink[i].addEventListener('click', () => {
-		change([navLink, items], i)
-	})
+// Перебираем элементы ссылок
+for (let i = 0; i < navLinkNav.length; i++) {
+  navLinkNav[i].addEventListener('click', () => {
+    isActive([navLinkNav, itemsTabs], i)
+  })
 }
 
+// Табы выбора карточек
 
-// Табы выборка карточек
+let tabsNavCards = document.querySelector('.cards-slider') // Родитель линков
+let tabsContentCards = document.querySelector('.cards__content') // Родитель таб-контента
+let navLinkCards = tabsNavCards.querySelectorAll('.cards-slider__slide') // Линки
+let itemsCards = tabsContentCards.querySelectorAll('.category__item') // Таб-контент
 
-let tabsNav2 = document.querySelector('.cards-slider') // Родитель линков
-let tabsContent2 = document.querySelector('.cards__content') // Родитель таб-контента
-let navLink2 = tabsNav2.querySelectorAll('.cards-slider__slide')   // Линки
-let items2= tabsContent2.querySelectorAll('.category__item') // Таб-контент
 
-function change(arr, i) {
-	arr.forEach( item => {
-		item.forEach( i => {i.classList.remove('is-active')})
-		item[i].classList.add('is-active')
-	})
-}
-
-for(let i = 0; i < navLink2.length; i++) {
-	navLink2[i].addEventListener('click', () => {
-		change([navLink2, items2], i)
-	})
+for (let i = 0; i < navLinkCards.length; i++) {
+  navLinkCards[i].addEventListener('click', () => {
+    isActive([navLinkCards, itemsCards], i)
+  })
 }
